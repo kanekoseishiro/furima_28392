9bb72c596eb7ae632651cbe81be3b25f97af13e1
kanekoseishiro <sei1025kaneko@gmail.com>
2020-08-21 19:03:50 +0900

    テストコード作成

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,13 +1,14 @@
 class User < ApplicationRecord
   with_options presence: true do
-    character = "/\A[ぁ-んァ-ン一-龥]/"
-    kana = "/\A[ァ-ン]/"
+    character = /\A[ぁ-んァ-ン一-龥]/
+    kana = /\A[ァ-ン]/
+    validates :nickname, length: { maximum: 40 }
     validates :first_name, format: { with: character}
     validates :last_name, format: { with: character}
     validates :first_name_kana, format: { with: kana}
     validates :last_name_kana, format: { with: kana}
     validates :birthday
-    validates :password, format: { with: /\A(?=.?[a-z])(?=.?\d)[a-z\d]+\z/i }
+    validates :password, format: { with: /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]+\z/i }, length: { minimum: 6 }
   end
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
